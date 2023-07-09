4651b2e2dccc1bc69e191c419bf5ecedb8b76dbf
ludwig-graef <ludwig.graef98@gmail.com>
2023-07-09 23:56:27 +0200

    Feature Extraction with RESNET

--- utils/general.py
+++ utils/general.py
@@ -8,9 +8,5 @@ def imshow(img: torch.Tensor):
     """
    shows an imagenet-normalized image on the screen
    """
-    mean = torch.tensor([0.485, 0.456, 0.406], dtype=torch.float32)
-    std = torch.tensor([0.229, 0.224, 0.225], dtype=torch.float32)
-    unnormalize = T.Normalize((-mean / std).tolist(), (1.0 / std).tolist())
-    npimg = unnormalize(img).numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.imshow(np.transpose(img.numpy(), (1, 2, 0)))
     plt.show()
